89ef8ed22bb45768d23219b72e25600fb800118e
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-08-11 18:17:26 +0530

    Create Reinforce_Cipher.py

---
+++ Reinforce_Cipher.py
@@ -0,0 +1,19 @@
+#Reinforce Cipher
+def Reinforce_Cipher(s):
+    n = len(s)
+    print(n)
+    arr = [0] * len(s)
+    j = 0
+    for i in range(0,n-1,2):
+        arr[j] = s[i]
+        arr[n//2 + j] = s[i+1]
+        j += 1
+    
+    if n % 2 != 0:
+        arr.insert(j,s[-1])
+        arr.pop()
+        
+    return arr
+    
+s = input()
+print(Reinforce_Cipher(s))
