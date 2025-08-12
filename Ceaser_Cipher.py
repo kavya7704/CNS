fccd38907bffe53335c022bf8fd941c3f7a6dc59
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-08-12 18:55:12 +0530

    Update Ceaser_Cipher.py

--- Ceaser_Cipher.py
+++ Ceaser_Cipher.py
@@ -3,13 +3,13 @@ def Ceaser_Cipher(s,k):
     
     new = ''
     for i in s:
-        temp = chr((ord(i) + k))
+        temp = chr((((ord(i) - 65 + k)) % 26)+65)
         new += temp
         
     return new
     
-s = input()
-k = int(input())
+s = "HELLO"
+k = 88
 print(Ceaser_Cipher(s,k))
 
 #Decoding
@@ -17,7 +17,7 @@ def Ceaser_Cipher(s,k):
     
     new = ''
     for i in s:
-        temp = chr((ord(i) - k))
+        temp = chr((((ord(i) - 65 - k)) % 26)+65)
         new += temp
         
     return new
